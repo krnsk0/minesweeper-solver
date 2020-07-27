d20bace137a3c14deb77049d4df39dc324cb6757
krnsk0 <jkurinsky@gmail.com>
2020-07-27 18:11:20 -0400

    refactor wip

--- ms.js
+++ ms.js
@@ -90,46 +90,7 @@ const handleZeros = (m) => {
   }
 };
 
-const isCellPossibleSolvable = (m, row, col) => {
-  if (m[row][col].value === '?') {
-    const surroundingCellRefs = getSurroundingCellRefs(
-      row,
-      col,
-      m.width,
-      m.height
-    );
-    return surroundingCellRefs.some(
-      ([r, c]) => !['?', '.'].includes(m[r][c].value)
-    );
-  }
-  return false;
-};
-
-markPoentialSolvables = (m) => {
-  m.forEach((row, rowIndex) => {
-    row.forEach((cell, colIndex) => {
-      if (isCellPossibleSolvable(m, rowIndex, colIndex)) {
-        m[rowIndex][colIndex].solvable = true;
-      }
-    });
-  });
-};
-
-getPotentialSolveableRefs = (m) => {
-  return m.reduce((outerAcc, row, rowIndex) => {
-    outerAcc.push(
-      ...row.reduce((innerAcc, cell, colIndex) => {
-        if (cell.solvable) {
-          const coords = [rowIndex, colIndex];
-          innerAcc.push(coords);
-        }
-        return innerAcc;
-      }, [])
-    );
-    return outerAcc;
-  }, []);
-};
-
+// eslint-disable-next-line no-unused-vars
 const generateBinaryStrings = (length, totalOnesAllowed) => {
   const output = [];
   const recurse = (length, str = '') => {
@@ -148,105 +109,11 @@ const generateBinaryStrings = (length, totalOnesAllowed) => {
   return output;
 };
 
-const resetHypotheses = (m) => {
-  m.forEach((row) => {
-    row.forEach((col) => {
-      if (col.hypothesis) delete col.hypothesis;
-      if (col.solvable) delete col.solvable;
-    });
-  });
-};
-
-const testClue = (m, [r, c]) => {
-  const mineCount = getSurroundingCellRefs(r, c, m.width, m.height).reduce(
-    (count, [r, c]) => {
-      if (m[r][c].hasOwnProperty('hypothesis') && m[r][c].hypothesis === 'x')
-        count += 1;
-      else if (m[r][c].value === 'x') count += 1;
-      return count;
-    },
-    0
-  );
-
-  const clue = parseInt(m[r][c].value);
-  if (mineCount > clue) {
-    return 'greater';
-  }
-  if (mineCount < clue) {
-    return 'less';
-  }
-  return 'equal';
-};
-
-const getClueRefs = (m) => {
-  const clueRefs = [];
-  m.forEach((row, rowIndex) => {
-    row.forEach((cell, colIndex) => {
-      if (!isNaN(parseInt(cell.value))) {
-        clueRefs.push([rowIndex, colIndex]);
-      }
-    });
-  });
-  return clueRefs;
-};
-
-const testHypothesis = (m) => {
-  const clueRefs = getClueRefs(m);
-  return clueRefs.every((clueRef) => {
-    const result = testClue(m, clueRef);
-    return result === 'less' || result === 'equal';
-  });
-};
-
-const inferFromPermutations = (validPerms) => {
-  console.log('validPerms: ', validPerms);
-  const stringLength = validPerms[0].length;
-  const output = Array.from({ length: stringLength });
-
-  for (let i = 0; i < stringLength; i += 1) {
-    const allX = validPerms.every((perm) => {
-      return perm[i] === 'x';
-    });
-    const allDash = validPerms.every((perm) => {
-      return perm[i] === '-';
-    });
-    console.log('allDash: ', allDash);
-    console.log('allX: ', allX);
-    if (allX || allDash) {
-      output[i] = validPerms[0][i];
-    }
-  }
-
-  console.log('output: ', output);
-};
-
-const permutateAllSolvables = (m) => {
-  const solvables = getPotentialSolveableRefs(m);
-  const permutations = generateBinaryStrings(solvables.length, m.n);
-  const validPerms = permutations.filter((permutation) => {
-    // plug in hypothesis
-    permutation.split('').forEach((permCell, permIndex) => {
-      const [row, col] = solvables[permIndex];
-      m[row][col].hypothesis = permCell;
-    });
-
-    // test hypothesis
-    const testResult = testHypothesis(m);
-    // console.log(`testing ${permutation} ; result ${testResult}`);
-    return testResult;
-  });
-  inferFromPermutations(validPerms);
-};
-
 const solveMine = (map, n) => {
   // initialization
   const m = parseMap(map, n);
 
   handleZeros(m);
-
-  markPoentialSolvables(m);
-
-  permutateAllSolvables(m);
 
   return printMap(m);
 };
